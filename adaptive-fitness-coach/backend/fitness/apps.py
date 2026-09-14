8a805a249442326be88df0a90243aa81f842949c
alenzjaison-source <alenzjaison@gmail.com>
2026-09-15 00:13:16 +0530

    Delete adaptive-fitness-coach directory

--- adaptive-fitness-coach/backend/fitness/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class FitnessConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fitness'
-
-    def ready(self):
-        # Register signals if needed
-        pass
